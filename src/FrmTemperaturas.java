358580fef95852ffde042b6d15c7ee8c8c79b58d
Yeison Alexis <yeisonalexis2711@gmail.com>
2025-05-10 17:19:31 -0500

    Inicializacion

---
+++ src/FrmTemperaturas.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class FrmTemperaturas extends JFrame {
+
+    public FrmTemperaturas() {
+        setTitle("Temperaturas");
+        setSize(700, 400);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
